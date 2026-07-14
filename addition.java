c547f2ddb662f2b1c0a15c016cb649acda1d8905
Pavan Khot <pavankhot261@gmail.com>
2026-07-14 10:51:52 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,13 @@
+public class addition {
+
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 12;
+
+        System.out.println("Number 1 is " + a);
+        System.out.println("Number 2 is " + b);
+
+        int c = a + b;
+        System.out.println("Addition of " + a + " and " + b + " is " + c);
+    }
+}
